74ccd76bb65a0bd720a8303e35bb1f294c3481ea
ponentesincausa <ponentesincausa@gmail.com>
2017-03-20 16:17:11 -0600

    agrega tercera sección

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,12 @@
 class WelcomeController < ApplicationController
 	def index
-		@titles = Title.order("RANDOM()").limit(2)
+		@titles = Title.order("RANDOM()").limit(3)
 
    		respond_to do |format|
 		    format.html
-		    format.json { 
+		    format.json {
 		    	format.json  { render :json => @titles }
-	     	} 
+	     	}
    		end
 	end
 end
