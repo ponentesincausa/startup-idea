4d48f8bdc21c75f7d3f4ec06e1a4cfa5f82c79e7
ponentesincausa <ponentesincausa@gmail.com>
2017-03-29 20:27:36 -0600

    modifica welcome controller para json e index.html, da error al segundo request

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,29 +1,32 @@
 class WelcomeController < ApplicationController
 	def index
 		@titles = Title.order("RANDOM()").limit(1)
-
+		@trends = Trend.order("RANDOM()").limit(1)
+		@problems = Problem.order("RANDOM()").limit(1)
+		response = { :titles => @titles, :trends => @trends, :problems => @problems }
    		respond_to do |format|
 		    format.html
-		    format.json {
-		    	format.json  { render :json => @titles }
-	     	}
+				puts "aquí debería de ir algo: #{@trends}"
+				format.json { render :json => response }
+				# format.json { render :json => @trends }
+				# format.json { render :json => @problems }
    		end
 
-		@trends = Trend.order("RANDOM()").limit(1)
 
-				respond_to do |format|
-					format.html
-					format.json {
-						format.json  { render :json => @trends }
-					}
-			end
-		@problems = Problem.order("RANDOM()").limit(1)
-
-				respond_to do |format|
-					format.html
-					format.json {
-						format.json  { render :json => @problems }
-					}
-			end
+			#
+			# 	respond_to do |format|
+			# 		format.html
+			# 		format.json {
+			# 			format.json  { render :json => @trends }
+			# 		}
+			# end
+			#
+			#
+			# 	respond_to do |format|
+			# 		format.html
+			# 		format.json {
+			# 			format.json  { render :json => @problems }
+			# 		}
+			# end
 	end
 end
